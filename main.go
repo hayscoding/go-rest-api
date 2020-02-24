d6b8c65cf568b2d54b460703965b02b47f2066d5
Hays Stanford <stanfordhays@gmail.com>
2020-02-24 14:21:34 -0600

    changed Inventory to a Slice data structure & implement a mock data store in main()

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ type Item struct {
 	Price float64 `json:"Price"`
 }
 
-type Inventory []Item
+var inventory []Item
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Function Called: homePage()")
@@ -24,10 +24,6 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func getInventory(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-
-	inventory := Inventory{
-		Item{UID: "0", Name: "Cheese", Desc: "A fine block of cheese.", Price: 4.99},
-	}
 
 	fmt.Println("Function Called: getInventory()")
 	json.NewEncoder(w).Encode(inventory)
@@ -40,7 +36,7 @@ func createItem(w http.ResponseWriter, r *http.Request) {
 func handleRequests(){
 	// := is the short variable declaration operator
 	// Automatically determines type for variable
-	router := mux.NewRouter().StrictSlash(true)
+	router := mux.NewRouter().StrictSlash(true)	
 
 	router.HandleFunc("/", homePage).Methods("GET")
 	router.HandleFunc("/inventory", getInventory).Methods("GET")
@@ -50,5 +46,13 @@ func handleRequests(){
 }
 
 func main() {
+	// Data store
+	inventory = append(inventory, Item{
+		UID: "0", 
+		Name: "Cheese", 
+		Desc: "A fine block of cheese.",
+		Price: 4.99,
+	}) 
+
 	handleRequests()
 }
